f3914b08465a5009cd979bfe3f5f36cd3117eadb
elmonstro <jratcher@gmail.com>
2022-07-07 09:11:36 +0300

    Create individual containers for each card group

--- src/components/orgStructureContAdmin/index.js
+++ src/components/orgStructureContAdmin/index.js
@@ -1,33 +1,22 @@
 import React from "react";
+
+import "./index.scss";
 import MissionVisionForm from "../cards/missionVisonCard";
 import UploadLogoCard from "../cards/uploadLogoCard";
 import UploadOrgCard from "../cards/uploadOrgCard";
 import DivisionsNamesForm from "../cards/orgNamesCard";
-
-
-import "./index.scss";
 import ThresholdsForm from "../cards/threshholdsCard";
-import PesrpectiveSwitchForm from "../cards/enablePerspectivesCard";
-import BehavioralSwitchForm from "../cards/enableBehavioralsCard";
-import CascadeCutoffForm from "../cards/cascadeCutoffCard";
-import ReviewPeriodCard from "../cards/reviewPeriodCard";
-import EditPerspectivesCard from "../cards/editPerspectiveCard";
+
 
 const AdminOrgStructureCont = props => {
     
     return (
-        <div className="admin_org_cont">
-            <EditPerspectivesCard />
+        <div className="admin_cont">
             <UploadLogoCard />
             <MissionVisionForm />
             <UploadOrgCard />
             <DivisionsNamesForm />
             <ThresholdsForm />
-            <PesrpectiveSwitchForm />
-            <BehavioralSwitchForm />
-            <CascadeCutoffForm />
-            <ReviewPeriodCard />
-
         </div>
     )
 }
