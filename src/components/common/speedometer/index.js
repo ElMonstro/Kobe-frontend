2f39e8e72051e3be3f9b2e956399bc03fa06d9e8
elmonstro <jratcher@gmail.com>
2022-11-27 14:49:32 +0300

    Add speedometers

--- src/components/common/speedometer/index.js
+++ src/components/common/speedometer/index.js
@@ -1,96 +1,35 @@
-import React from "react";
-import { Chart } from "chart.js";
-import { useEffect } from "react";
+import { Card } from 'react-bootstrap';
+import GaugeChart from 'react-gauge-chart';
 
-import "./styles.scss";
 import { connect } from "react-redux";
+import "./index.scss"
 
-const Speedometer =  ({ data, value, upper_threshold, lower_threshold }) => {
+const Speedometer =  ({ arcsLength, percent, id, title, upper_threshold, lower_threshold}) => {
 
-    if (data === undefined) {
-        data = [lower_threshold, upper_threshold, 100];
+    if (arcsLength === undefined) {
+        arcsLength = [lower_threshold/100, upper_threshold/100, 1-upper_threshold/100];
     }
 
-    const config = {
-        type: "gauge",
-        data: {
-          labels: ["Normal", "Warning", "Critical"],
-          datasets: [
-            {
-              label: "Current Appeal Risk",
-              data: data,
-              value: value,
-              minValue: 0,
-              backgroundColor: ["green", "orange", "red"],
-              borderWidth: 7
-            }
-          ]
-        },
-        options: {
-          legend: {
-            display: true,
-            position: "bottom",
-            labels: {
-              generateLabels: {}
-            }
-          },
-          responsive: true,
-          title: {
-            display: true,
-            text: "Financial Risk"
-          },
-          layout: {
-            padding: {
-              bottom: 20
-            }
-          },
-          needle: {
-            radiusPercentage: 1,
-            widthPercentage: 1,
-            lengthPercentage: 60,
-            color: "rgba(0, 0, 0, 1)"
-          },
-          valueLabel: {
-            fontSize: 12,
-            formatter: function (value, context) {
-              return value + " %";
-            }
-          },
-          plugins: {
-            datalabels: {
-              display: "auto",
-              formatter: function (value, context) {
-                return context.chart.data.labels[context.dataIndex];
-      
-              },
-              color: function (context) {
-                return "#ffff";
-              },
-          
-              font: function (context) {
-                let width = context.chart.width;
-                let size = Math.round(width / 36);
-      
-                return {
-                  weight: "normal",
-                  size: size
-                };
-              }
-            }
-          }
-        }
-      };
-
-  useEffect(() => {
-    let ctx = document.getElementById("myChart").getContext("2d");
-    const myGauge = new Chart(ctx, config);
-    myGauge.update();
-  }, []);
+    const style = {
+      width: "500px",
+    }
 
   return (
-    <div className="speedometer">
-      <canvas id="myChart"></canvas>
-    </div>
+    <Card className="speedometer staff_card">
+      <Card.Header>
+        <Card.Title className="title">{ title }</Card.Title>
+      </Card.Header>
+      <GaugeChart 
+        id={ id } 
+        cornerRadius={ 0 } 
+        arcsLength={ arcsLength }
+        percent = { percent }
+        style={ style }
+        hideText
+        arcWidth={0.3}
+      />
+      <span className="percentage">{ percent * 100 } %</span>
+    </Card>
   );
 }
 
--- src/components/dashboardsTab/index.js
+++ src/components/dashboardsTab/index.js
@@ -3,6 +3,7 @@ import { Col, Row } from "react-bootstrap";
 import { useOutletContext } from "react-router-dom";
 
 import { DASHBOARDS } from "../../utils/constants";
+import Speedometer from "../common/speedometer";
 import DashboardsSidebar from "./dashboardsSidebar";
 import "./index.scss";
 
@@ -16,10 +17,18 @@ const DashboardTab = () => {
 
     return (
         <Row className="dashboards_tab">
-            <Col lg="2" className="dashboards_sidebar_cont">
+            <Col lg="3" className="dashboards_sidebar_cont">
                 <DashboardsSidebar />
             </Col>
             <Col className="charts">
+                <Row>
+                    <Col className="actual">
+                        <Speedometer title="Actual" percent={0.45} />
+                    </Col>
+                    <Col className="planned">
+                        <Speedometer title="Planned" percent={0.21} />
+                    </Col>
+                </Row>
             </Col>
         </Row>
     )
