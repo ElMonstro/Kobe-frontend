1e4f4781aed40f83fa206f98332b53df546b40d3
elmonstro <jratcher@gmail.com>
2023-01-17 11:22:44 +0300

    Change url to server url

--- src/services/baseURL.js
+++ src/services/baseURL.js
@@ -1,10 +1,10 @@
 const resolve_base_url = () => {
-    const base_url = "http://127.0.0.1:8000/api/v1";
-    // const base_url = "http://206.189.125.52:8000/api/v1";
+    // const base_url = "http://127.0.0.1:8000/api/v1";
+    const base_url = "http://144.126.192.148:8000/api/v1";
 
     return base_url;
 }
 export const base_cloudinary_url = "https://res.cloudinary.com/elmonstro/";
 export default resolve_base_url;
-// export const sockets_url = "ws://206.189.125.52:8000/ws";
-export const sockets_url = "ws://127.0.0.1:8000/ws";
+export const sockets_url = "ws://144.126.192.148:8000/ws";
+// export const sockets_url = "ws://127.0.0.1:8000/ws";
