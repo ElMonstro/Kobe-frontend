b215781ddf57b6c378194b2e626ec83e6fc1cab3
elmonstro <jratcher@gmail.com>
2024-03-05 19:31:12 +0300

    Build RELEASE

--- src/services/baseURL.js
+++ src/services/baseURL.js
@@ -1,6 +1,6 @@
 const resolve_base_url = () => {
-    let base_url = "http://127.0.0.1:8000/api/v1";
-    // let base_url = "http://178.62.1.20:8000/api/v1";
+    // let base_url = "http://127.0.0.1:8000/api/v1";
+    let base_url = "http://178.62.1.20:8000/api/v1";
 
     const baseURL = window.localStorage.getItem('baseURL');
     baseURL && (base_url = baseURL);
