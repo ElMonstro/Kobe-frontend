3cc6942db645e5964bd098eba05dcc5af772427d
elmonstro <jratcher@gmail.com>
2024-03-05 09:56:22 +0300

    Fix perspective weight settings

--- src/services/baseURL.js
+++ src/services/baseURL.js
@@ -1,6 +1,6 @@
 const resolve_base_url = () => {
-    // let base_url = "http://127.0.0.1:8000/api/v1";
-    let base_url = "http://178.62.1.20:8000/api/v1";
+    let base_url = "http://127.0.0.1:8000/api/v1";
+    // let base_url = "http://178.62.1.20:8000/api/v1";
 
     const baseURL = window.localStorage.getItem('baseURL');
     baseURL && (base_url = baseURL);
@@ -10,4 +10,4 @@ const resolve_base_url = () => {
 export const BASE_CLOUDINARY_URL = "https://res.cloudinary.com/elmonstro/";
 export default resolve_base_url;
 export const sockets_url = "ws://144.126.192.148:8000/ws";
-// export const sockets_url = "ws://127.0.0.1:8000/ws";
+// export const sockets_url = "ws://127.0.0.1:8000/ws"
